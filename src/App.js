394afae43ad59dedc0ee6c53b2939c8d56a15f02
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-04-14 12:13:48 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLodding, setIsLoadding] = useState(false);
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoadding(true);
+    const response = await fetch("https://skill-http-default-rtdb.firebaseio.com/movies.json");
+    const data = await response.json();
+
+    const transformData = data.results.map((moviesData) => {
+      return {
+        id: moviesData.episode_id,
+        title: moviesData.title,
+        openingText: moviesData.opening_crawl,
+        releaseData: moviesData.release_date,
+      };
+    });
+    setMovies(transformData);
+    setIsLoadding(false);
+  }, []);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://skill-http-default-rtdb.firebaseio.com/movies.json',{
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLodding && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLodding && movies.length === 0 && <p>Found No Movies.</p>}
+        {isLodding && <p>Lodding...</p>}
       </section>
     </React.Fragment>
   );
