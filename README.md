3f4943effc9f7f2f3fac829d82e8fa71c6e7c6ec
Shivangi Gupta <71278498+Shivangi-2000@users.noreply.github.com>
2023-04-14 13:36:04 +0530

    add movies

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # React_With_Database
+
 API URL
 https://swapi.py4e.com/
+
+Firebase API
+project name: skill-http
+https://skill-http-default-rtdb.firebaseio.com/movies.json
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,11 +1,37 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-import classes from './AddMovie.module.css';
+import classes from "./AddMovie.module.css";
 
 function AddMovie(props) {
-  const titleRef = useRef('');
-  const openingTextRef = useRef('');
-  const releaseDateRef = useRef('');
+  const [isValid, setIsValid] = useState(false);
+  const [istitleRef, setIstitleRef] = useState("");
+  const [isopeningTextRef, setIsopeningTextRef] = useState("");
+  const [isreleaseDateRef, setIsreleaseDateRef] = useState("");
+  const titleRef = useRef("");
+  const openingTextRef = useRef("");
+  const releaseDateRef = useRef("");
+
+  const goalInputChangeHandler1 = (event) => {
+    setIstitleRef(event.target.value);
+  };
+  const goalInputChangeHandler2 = (event) => {
+    setIsopeningTextRef(event.target.value);
+  };
+  const goalInputChangeHandler3 = (event) => {
+    setIsreleaseDateRef(event.target.value);
+  };
+  useEffect(() => {
+    if (
+      istitleRef.trim().length > 0 &&
+      isopeningTextRef.trim().length > 0 &&
+      isreleaseDateRef.trim().length > 0
+    ) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+    console.log("isValid", isValid);
+  }, [istitleRef, isopeningTextRef, isreleaseDateRef]);
 
   function submitHandler(event) {
     event.preventDefault();
@@ -19,23 +45,46 @@ function AddMovie(props) {
     };
 
     props.onAddMovie(movie);
+    //reset the values
+    titleRef.current.value = "";
+    openingTextRef.current.value = "";
+    releaseDateRef.current.value = "";
+    setIstitleRef(titleRef.current.value);
+    setIsopeningTextRef(openingTextRef.current.value);
+    setIsreleaseDateRef(releaseDateRef.current.value);
   }
 
   return (
     <form onSubmit={submitHandler}>
+      <h1>Add Movie</h1>
       <div className={classes.control}>
-        <label htmlFor='title'>Title</label>
-        <input type='text' id='title' ref={titleRef} />
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          ref={titleRef}
+          onChange={goalInputChangeHandler1}
+        />
       </div>
       <div className={classes.control}>
-        <label htmlFor='opening-text'>Opening Text</label>
-        <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
+        <label htmlFor="opening-text">Opening Text</label>
+        <textarea
+          rows="5"
+          id="opening-text"
+          ref={openingTextRef}
+          onChange={goalInputChangeHandler2}
+        ></textarea>
       </div>
       <div className={classes.control}>
-        <label htmlFor='date'>Release Date</label>
-        <input type='text' id='date' ref={releaseDateRef} />
+        <label htmlFor="date">Release Date</label>
+        <input
+          type="text"
+          id="date"
+          ref={releaseDateRef}
+          onChange={goalInputChangeHandler3}
+        />
       </div>
-      <button>Add Movie</button>
+      {isValid && <button>Add Movie</button>}
     </form>
   );
 }
